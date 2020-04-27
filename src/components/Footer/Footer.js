af59d14fe48fbfe2bd56b9475eeb0ab29c24594a
Vasilya Nizamova <victoriartz89@gmail.com>
2020-04-27 10:32:44 -0400

    add images

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Footer.css'
+import Fb from "../../Images/Facebook.svg"
+import Ig from "../../Images/Instagram.svg"
 
 const Footer = () => {
   return (
     <footer>
       <h2>This is a footer</h2>
+      <img src={Fb} />
+      <img src={Ig} />
     </footer>
   )
 }
--- src/components/ImageArray/ImageArray.js
+++ src/components/ImageArray/ImageArray.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './ImageArray.css'
+import Food1 from "../../Images/Food-1.png"
+import Food2 from "../../Images/Food-2.png"
+import Food3 from "../../Images/Food-3.png"
+import Food4 from "../../Images/Food-4.png"
 
 const ImageArray = () => {
   return (
     <div>
-      <img src='' alt='img' />
-      <img src='' alt='img' />
-      <img src='' alt='img' />
-      <img src='' alt='img' />
+      <img src={Food1} alt='dish one' />
+      <img src={Food2} alt='dish two' />
+      <img src={Food3} alt='dish three' />
+      <img src={Food4} alt='dish four' />
     </div>
   )
 }
