d1f47b5cb83243c0361aa55ac05c04c0550f77ce
jasonc1642 <jason1642@gmail.com>
2020-04-27 12:06:19 -0400

    menu state and props start

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Menu.css'
+import { Component } from 'react'
 import { Link, Route } from 'react-router-dom'
 import MenuBrunch from '../MenuBrunch/MenuBrunch'
 import MenuLunch from '../MenuLunch/MenuLunch'
@@ -9,23 +10,33 @@ import Menus from './Menus.json'
 
 console.log(Menus);
 
-const Menu = () => {
-  return (
-    <div className='menu-container'>
-      <nav>
-        <Link to='/' >Brunch</Link>
-        <Link to='/lunch-menu' >Lunch</Link>
-        <Link to='/dinner-menu' >Dinner</Link>
-        <Link to='/dessert-menu' >Dessert</Link>
-      </nav>
-      <div className='current-menu'>
-        <Route exact path='/' render={MenuBrunch} />
-        <Route path='/lunch-menu' render={MenuLunch} />
-        <Route path='/dinner-menu' render={MenuDinner} />
-        <Route path='/dessert-menu' render={MenuDessert} />
+class Menu extends Component {
+  constructor() {
+    super()
+    this.state = {
+      menus: Menus
+    }
+  }
+  render() {
+    console.log(this.state.menus.Dinner)
+    return (
+      <div className='menu-container'>
+        <nav>
+          <Link to='/' >Brunch</Link>
+          <Link to='/lunch-menu' >Lunch</Link>
+          <Link to='/dinner-menu' >Dinner</Link>
+          <Link to='/dessert-menu' >Dessert</Link>
+        </nav>
+        <div className='current-menu'>
+          <Route exact path='/' render={MenuBrunch} />
+          <Route path='/lunch-menu' render={MenuLunch} />
+          <Route path='/dinner-menu' render={() =>
+            <MenuDinner menu={this.state.menus.Dinner} />} />
+          <Route path='/dessert-menu' render={MenuDessert} />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Menu
