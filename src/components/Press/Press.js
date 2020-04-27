4222933526cc5a706e840a0bdcd3a9b5b66f4326
Vasilya Nizamova <victoriartz89@gmail.com>
2020-04-27 12:13:34 -0400

    add links

--- src/components/Press/Press.js
+++ src/components/Press/Press.js
@@ -8,10 +8,10 @@ import Press4 from "../../Images/Press-4.png"
 const Press = () => {
   return (
     <press>
-      <img src={Press1} alt='press image' />
-      <img src={Press2} alt='press image' />
-      <img src={Press3} alt='press image' />
-      <img src={Press4} alt='press image' />
+      <a href="https://tmagazine.blogs.nytimes.com/2014/08/28/al-di-la-vino-sunday-night-dinner/"><img src={Press1}/></a>
+      <a href="https://www.cntraveler.com/restaurants/brooklyn/al-di-la-trattoria"><img src={Press2}/></a>
+      <a href="https://www.timeout.com/newyork/restaurants/al-di-la-vino"><img src={Press3}/></a>
+      <a href="https://www.wsj.com/articles/brooklyn-restaurant-reaches-far-with-challenging-wines-1379037898"><img src={Press4}/></a>
     </press>
   )
 }
