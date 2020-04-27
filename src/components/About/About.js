6149bc80b6e3f0bb68a6965ea9ee79f82aacbef6
Vasilya Nizamova <victoriartz89@gmail.com>
2020-04-27 15:21:01 -0400

    about and visit comp style

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './About.css'
+import AboutImg from "../../Images/About.png"
 
 const About = () => {
   return (
+    <about>
     <div className='about-box'>
-      <h2 className='about-description'>
-        Rooted in the traditions of Northern Italian cuisine, al di l&agrave; has been a Park Slope staple for over ten years.
-      </h2>
-      <h2 className='about-description'>
-        We are owned and operated by husband and wife team Emiliano Coppa and Chef Anna Klinger.
-      </h2>
+      <h2>Rooted in the traditions of Northern Italian cuisine, al di l&agrave; has been a Park Slope staple for over ten years.</h2>
+      <h2>We are owned and operated by husband and wife team Emiliano Coppa and Chef Anna Klinger.</h2>
     </div>
+    <img className='about-image' src={AboutImg}/>
+    </about>
   )
 }
 
