b1d92f87b7da997a0a360da234754586eb3ebdd9
jasonc1642 <jason1642@gmail.com>
2020-04-28 20:30:11 -0400

    stash

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,87 +1,134 @@
-import React, { Component } from "react";
-import { Link, animateScroll as scroll } from "react-scroll";
+import React,
+  {
+  Component
+}
+
+from "react";
+
+import {
+  Link,
+  animateScroll as scroll
+}
+
+from "react-scroll";
 import Logo from '../../Images/al_di_la_logo.svg'
 import Hero from '../../Images/Hero.png'
 import "../Header/Header.css"
 
 export default class Header extends Component {
-  scrollToTop = () => {
+  scrollToTop=()=> {
     scroll.scrollToTop();
-  };
+  }
+
+  ;
 
   render() {
-    return (
-      <header>
-      <nav className="nav" id="navbar">
-        <div className="nav-content">
-          
-          <ul className="nav-items">
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="section1"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                menu
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="section2"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                visit
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="section3"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                <img src={Logo} alt="logo"/>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="section4"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                about
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="section5"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                press
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <div style={{ backgroundImage: "url(" + Hero + ")", backgroundSize: "cover", height: "80vh" }}></div>
-      </header>
-    );
+    return (<header> <nav className="nav"id="navbar"> <div className="nav-content"> <ul className="nav-items"> <li className="nav-item"> <Link activeClass="active"
+      to="section1"
+
+      spy= {
+        true
+      }
+
+      smooth= {
+        true
+      }
+
+      offset= {
+        -70
+      }
+
+      duration= {
+        500
+      }
+
+      > menu </Link> </li> <li className="nav-item"> <Link activeClass="active"
+      to="section2"
+
+      spy= {
+        true
+      }
+
+      smooth= {
+        true
+      }
+
+      offset= {
+        -70
+      }
+
+      duration= {
+        500
+      }
+
+      > visit </Link> </li> <li className="nav-item"> <Link activeClass="active"
+      to="section3"
+
+      spy= {
+        true
+      }
+
+      smooth= {
+        true
+      }
+
+      offset= {
+        -70
+      }
+
+      duration= {
+        500
+      }
+
+      > <img src= {
+        Logo
+      }
+
+      alt="logo"/> </Link> </li> <li className="nav-item"> <Link activeClass="active"
+      to="section4"
+
+      spy= {
+        true
+      }
+
+      smooth= {
+        true
+      }
+
+      offset= {
+        -70
+      }
+
+      duration= {
+        500
+      }
+
+      > about </Link> </li> <li className="nav-item"> <Link activeClass="active"
+      to="section5"
+
+      spy= {
+        true
+      }
+
+      smooth= {
+        true
+      }
+
+      offset= {
+        -70
+      }
+
+      duration= {
+        500
+      }
+
+      > press </Link> </li> </ul> </div> </nav> <div style= {
+          {
+          backgroundImage: "url("+ Hero + ")", backgroundSize: "cover", height: "80vh"
+        }
+      }
+
+      ></div> </header>);
   }
 }
 
