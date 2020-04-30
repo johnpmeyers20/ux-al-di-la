86259c02f324b43b84f02738617de8c95d13140b
JP Meyers <johnpmeyers20@yahoo.com>
2020-04-29 22:44:41 -0400

    made images from array responsive

--- src/components/ImageArray/ImageArray.js
+++ src/components/ImageArray/ImageArray.js
@@ -8,10 +8,18 @@ import Food4 from "../../Images/Food-4.png"
 const ImageArray = () => {
   return (
     <div className="image-array-container">
-      <img src={Food1} alt='dish one' />
-      <img src={Food2} alt='dish two' />
-      <img src={Food3} alt='dish three' />
-      <img src={Food4} alt='dish four' />
+      <div className="image-container">
+        <img src={Food1} alt='dish one' />
+      </div>
+      <div className="image-container">
+        <img src={Food2} alt='dish two' />
+      </div>
+      <div className="image-container">
+        <img src={Food3} alt='dish three' />
+      </div>
+      <div className="image-container">
+        <img src={Food4} alt='dish four' />
+      </div>
     </div>
   )
 }
