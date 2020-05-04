768437294549e6cc5c7a26cdbf55f2a862996010
jasonc1642 <jason1642@gmail.com>
2020-05-03 20:53:05 -0400

    menu media queries

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 import HamburgerMenu from '../../Images/menu-icon.svg';
 import './Burger.css'
 
 const Burger = ({ open, setOpen }) => {
   return (
     // <button className="nav-toggle" open={open} onClick={() => setOpen(!open)}>
-      <img className="nav-toggle" open={open} onClick={() => setOpen(!open)} src={HamburgerMenu}/>
+    <img className="nav-toggle" open={open} onClick={() => setOpen(!open)} src={HamburgerMenu} />
     // </button>
   );
 }
